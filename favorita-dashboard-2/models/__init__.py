1c418dd71ebc72cabbfe04f75a8e1dba3f40967d
Khadijah19 <161076122+Khadijah19@users.noreply.github.com>
2026-01-13 15:22:13 +0000

    Delete favorita-dashboard-2 directory

--- favorita-dashboard-2/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 11 17:38:55 2026
-
-@author: HP
-"""
-
--- favorita-dashboard-2/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jan 11 16:37:18 2026
-
-@author: HP
-"""
